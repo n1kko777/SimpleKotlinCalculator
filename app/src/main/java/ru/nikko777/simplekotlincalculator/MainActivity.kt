0bd81eb1522a30fa0c5744e57bf72dcde7684a86
n1kko777 <3734489@gmail.com>
2017-06-12 10:21:36 +0500

    Initial commit

---
+++ app/src/main/java/ru/nikko777/simplekotlincalculator/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.nikko777.simplekotlincalculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        this.textView.text = "Kotlin is connected!"
+    }
+}
---
+++ app/src/main/java/ru/nikko777/simplekotlincalculator/ViewModel.kt
@@ -0,0 +1,7 @@
+package ru.nikko777.simplekotlincalculator
+
+/**
+ * Created by User on 12.06.2017.
+ */
+class ViewModel {
+}
